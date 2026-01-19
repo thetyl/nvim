ef2311956411dfb978faa75595546c5c23cab76e
Tyl <tyldahl@gmail.com>
2026-01-18 19:36:59 -0500

    Fix bug in treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,18 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	config = function() 
-		require("nvim-treesitter.config").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc" },
-			sync_install = false,
-			highlight = { enable = true }
+	config = function()
+		local treesitter = require("nvim-treesitter")
+		treesitter.setup()
+		treesitter.install { "c", "lua", "vim", "vimdoc", "cpp" }
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "c", "lua", "vim", "vimdoc", "cpp" },
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end
 		})
 	end
 }
